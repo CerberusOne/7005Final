4bbaccbebb92bbc2c2254aee475b3390cb6768a1
Ben <benedict.sh.lo@gmail.com>
2017-11-13 15:00:33 -0800

    problems with references

--- Emulator/emulator.cpp
+++ Emulator/emulator.cpp
@@ -1,6 +1,7 @@
-#include "../A1/client.h"
-#include "../A1/server.h"
-#include "../A1/socketwrappers.h"
+#include "../Library/client.h"
+#include "../Library/server.h"
+#include "../Library/socketwrappers.h"
+#include "../Library/library.h"
 #include "emulator.h"
 
 #define CLIENTIP 0
@@ -19,27 +20,51 @@ bool getConfig();
 string config[BUFLEN];
 
 int main (int argc, char *argv[]) {
+    if(!getConfig()){
+      perror("Could not get configs");
+    }
 
-  Client *outTX = new Client(ParseString(config[SERVERIP]),config[SERVERPORT]);
-  Server *inTX = new Server(config[CLIENTPORT]);
+    string ipp = config[EMULATORIP];
+    const char *ip = ipp.c_str();
+    string portt = config[EMULATORPORT];
+    int port = atoi(portt.c_str());
 
-  if(getConfig){
+    Client *outTX = new Client(ip,port);
 
-  }
+    string svrport = config[SERVERPORT];
+    int srvport = atoi(svrport.c_str());
+
+    Server *inTX = new Server(srvport);
+
+    Cmd cmd;
+    int command = 0;
+    string ss = config[CLIENTIP];
+    char *filename;
+    cout << "Enter command: " << endl;
+    cin >> filename;
+    cmd = CreateCmd(command, filename);
+    outTX->SendCmd(cmd);
+
+    Cmd ccmd;
+
+    ccmd = inTX->WaitCommand();
+
+
+
 }
 bool getConfig(){
-  ifstream file;
-  file.open("config");
-  //check if file exists
-  if (!file){
-    perror("Files does not exist");
-  } else {
-    int i;
-    for(i=0; i <SERVERPORT; i++){
-      getline(file, config[i]);
-      cout << config[i];
-    }
-    return true;
-  }
+     ifstream file;
+     file.open("config");
+     //check if file exists
+        if (!file){
+            perror("Files does not exist");
+        } else {
+            int i;
+                for(i=0; i <SERVERPORT; i++){
+                    getline(file, config[i]);
+                    cout << config[i];
+                }
+                return true;
+        }
   return false;
 }
--- Emulator/emulator.h
+++ Emulator/emulator.h
@@ -1,14 +1,17 @@
 #ifndef EMULATOR_H
 #define EMULATOR_H
 
-#include "../A1/socketwrappers.h"
-#include "../A1/client.h"
-#include "../A1/server.h"
+#include "../Library/socketwrappers.h"
+#include "../Library/client.h"
+#include "../Library/server.h"
+#include "../Library/library.h"
+#include <netinet/in.h>
 #include <iostream>
 #include <fstream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <cstdlib>
 
 using namespace std;
 
