c67241a16e73bd6c5f8847ff110326e0dac0681b
Ben <benedict.sh.lo@gmail.com>
2017-11-15 01:41:59 -0800

    Added select/timers/receive/send packet

--- Emulator/emulator.cpp
+++ Emulator/emulator.cpp
@@ -1,4 +1,4 @@
-#include "../Library/client.h"
+#include ":w../Library/client.h"
 #include "../Library/server.h"
 #include "../Library/socketwrappers.h"
 #include "../Library/library.h"
@@ -11,129 +11,206 @@
 #define EMULATORPORT2 4
 #define SERVERIP 5
 #define SERVERPORT 6
-#define NUM_THREADS 4
 
 #define PATH "./Client_files/"
 #define BUFLEN 1024
 
 using namespace std;
 
-void getConfig();
-int rando();
-bool same(int i,int arr[]);
+void GetConfig();
+int Rando();
+bool Same(int i,int arr[],int size);
 char *ParseString(string str);
-bool discard(int arr[]);
+bool Discard(int arr[],int size);
 
 string config[BUFLEN];
 
 int main (int argc, char *argv[]) {
-    pthread_t threads[NUM_THREADS];
-    int sc,cs,ss,cc;
-    int rate = atoi(argv[1]);
-    int BER[rate];
-    //check arguments
-    if (argc != 2){
-      perror("Error: ./file [BER]");
-    }
-    //get ip and port from config file
-    getConfig();
-    //populate BER array
-    int i;
-    for(i = 0; i<(sizeof(BER)/sizeof(int)); i++){
-        BER[i] = rando();
-        cout << BER[i] << endl;
-          if ((same(BER[i],BER)==TRUE)){
-            do {
-              BER[i] = rando();
-            } while (!same(BER[i],BER));
-          }
-      }
-      // test discard
-      /*size_t k;
-      for (k = 0; k<(sizeof(BER)/sizeof(int)); k++){
-        cout << discard(BER) << endl;
-      }*/
+	int rc;
+    	int rate = atoi(argv[1]);
+    	int BER[rate];
+    	//check arguments
+    	if (argc != 2){
+      		perror("Error: ./file [BER]");
+    	}
+    	//get ip and port from config file
+    	GetConfig();
+    	//populate BER array
+    	int i;
+    	for(i = 0; i<rate; i++){
+        	BER[i] = r\Rando();
+        	cout << BER[i] << endl;
+          	if ((Same(BER[i],BER))){
+            		do {
+              			BER[i] = Rando();
+            		} while (!Same(BER[i],BER));
+          	}
+      	}
+      	// test discard
+      	/*size_t k;
+      	for (k = 0; k<(sizeof(BER)/sizeof(int)); k++){
+        	cout << Discard(BER,rate) << endl;
+      	}*/
 
-    //for testing purposes
-    string ipemulator = config[SERVERIP];
-    const char *connectionIP = ipemulator.c_str();
+    	//for testing purposes
+    	string ipemulator = config[SERVERIP];
+    	const char *connectionIP = ipemulator.c_str();
 
-    /*
-    string porte = config[EMULATORPORT];
-    int eport = atoi(porte.c_str());
-    Server *serverinclient = new Server(eport);
+    	/*
+    	string porte = config[EMULATORPORT];
+    	int eport = atoi(porte.c_str());
+    	Server *serverinclient = new Server(eport);
 
-    string ips = config[SERVERIP];
-    const char *sip = ips.c_str();
-    string Ports = config[SERVERPORT];
-    int sPort = atoi(ports.c_str());
-    Client *clientoutserver = new Client(ips,sPort);
+    	string ips = config[SERVERIP];
+    	const char *sip = ips.c_str();
+    	string Ports = config[SERVERPORT];
+    	int sPort = atoi(ports.c_str());
+    	Client *clientoutserver = new Client(ips,sPort);
 
-    string porte2 = config[EMULATORPORT2];
-    int ePort2 = atoi(porte2.c_str());
-    Server *serverinserver = new Server(ePort2);
+    	string porte2 = config[EMULATORPORT2];
+    	int ePort2 = atoi(porte2.c_str());
+    	Server *serverinserver = new Server(ePort2);
 
-    string ipC = config[CLIENTIP];
-    const char *cIp = ipemulator.c_str();
-    string portC = config[CLIENTPORT];
-    int cPort = atoi(portC.c_str());
-    Client *clientoutclient = new Client(ipC, cPort);*/
-    sc = pthread_create(&threads[0], NULL, scStart, NULL);
-    cs =
-    ss =
-    cc =
-    Server *serverinclient = new Server(7006);
-    Client *clientoutserver = new Client(connectionIP,7008);
-    Server *serverinserver = new Server(7007);
-    Client *clientoutclient = new Client(connectionIP, 7005);
+    	string ipC = config[CLIENTIP];
+    	const char *cIp = ipemulator.c_str();
+    	string portC = config[CLIENTPORT];
+    	int cPort = atoi(portC.c_str());
+    	Client *clientoutclient = new Client(ipC, cPort);*/
+
+    	//set sockets to non-blocking
+    	Server *INclient = new Server(7006);
+    	rc = fcntl(INclient->GetSocket(),F_SETFL,O_NONBLOCK);
+    	if (rc < 0){
+	    	perrorperror("Error setting socket to non-blocking");
+	    	exit(3);
+    	}
+    	Client *OUTserver = new Client(connectionIP,7008);
+    	Server *INserver = new Server(7007);
+    	rc = fcntl(INserver->GetSocket(),F_SETFL,O_NONBLOCK);
+    	if (rc < 0){
+		perror("Error setting socket to non-blocking");
+	    	exit(3);
+    	}
+	Client *OUTclient = new Client(connectionIP, 7005);
+
+    	fd_set listeningset,master;
+    	//struct timeval timeout;
+    	int fdmax;
+    	//empty set
+    	FD_ZERO(&listeningset);
+    	FD_ZERO(&master);
+    	//Add sockets to set
+    	FD_SET(INclient->GetSocket(),&listeningset);
+    	FD_SET(INserver->GetSocket(),&listeningset);
+
+    	//track the biggest file descriptor
+    	fdmax = INserver->GetSocket();
+    	//set timeout value to one minute
+    	//timeout.tv_sec = 1 * 60;
+    	//timeout.tv_usec = 0;
+
+    	while(1){
+		clock_t start = clock();
+		//copy listeningset to master
+		memcpy(&master, &listeningset, sizeof(listeningset));
+
+	    	cout << "waiting for event" << endl;
+
+		//wait
+	    	rc = select (fdmax + 1, &master, NULL, NULL, NULL);
+	    	if (rc < 0){
+		    perror ("ERROR: Select()");
+		    exit(4);
+	    	}
+		//This can be used to see if it timed out can be used in the receiver
+		//if (rc == 0){	}
+
+	    	int i;
+	    	for (i = 0; i <= fdmax; i++){
+			//if descriptor is in the set
+			if (FD_ISSET(i, &master)){
+				double passed;
+				double delay = 60;
+				bool timer = true;
+				// start timer
+				while(timer){
+					passed = (clock() - start) / CLOCKS_PER_SEC;
+						if (passed >= delay){
+							cout << "Seconds have passed: " << passed << endl;
+							timer = false;
+						}
+				}
+				//If descriptor is coming from client
+				if (i == INclient->GetSocket()){
+					//If BER == 1 discard packet
+					if (!Discard(BER,rate)){
+						//Data from client will always be data or EOT
+						//Receive packet from client
+						RecvFile(INclient->GetSocket(),path);
+						//Send packet to server
+						SendFile(OUTclient->GetSocket(),path);
+					}
+
+				} else if(i == INserver->GetSocket()){
+					//If BER == 1 discard packet
+					if (!Discard(BER,rate)){
+					//Data from server will always be an Ack or EOT
+					//Receive packet from server
+					RecvFile(INserver->GetSocket(),path);
+					//Send Packet to client
+					SendFile(OUTserver->GetSocket(),path);
+					}
+
+				}
+			}
+	    }
 }
-void getConfig(){
-     ifstream file;
-     file.open("config");
-     //check if file exists
+void GetConfig(){
+	ifstream file;
+     	file.open("config");
+     	//check if file exists
         if (!file){
-            perror("Files does not exist");
-        } else {
-            int i;
+        	perror("Files does not exist");
+       	} else {
+        	int i;
                 for(i=0; i <SERVERPORT; i++){
-                    getline(file, config[i]);
-                    //cout << config[i] << endl;
+                	getline(file, config[i]);
+                    	//cout << config[i] << endl;
                 }
         }
 }
 
 char *ParseString(string str){
-    char *cstr;
+    	char *cstr;
 
-    cstr = new char[str.length() + 1];
-    strcpy(cstr, str.c_str());
+    	cstr = new char[str.length() + 1];
+    	strcpy(cstr, str.c_str());
 
-    return cstr;
+    	return cstr;
 }
 
-int rando(){
-  return rand() % 100 +1;
+int Rando(){
+  	return rand() % 100 +1;
 }
 
-bool same(int val,int arr[]){
-  size_t i;
-  for(i = 0; i<(sizeof(arr)/sizeof(int)); i++){
-      if (arr[i] == val){
-        return true;
-      }
-  }
+bool Same(int val,int arr[],int size){
+  	size_t i;
+  	for(i = 0; i<size; i++){
+      		if (arr[i] == val){
+        		return true;
+      		}	
+  	}
   return false;
 }
 
-bool discard(int arr[]){
-  size_t i;
-  int val = rando();
-  cout << "checking value with" << val << endl;
-  for(i = 0; i<(sizeof(arr)/sizeof(int)); i++){
-    if(same(val,arr)){
-      i = (sizeof(arr)/sizeof(int));
-      return true;
-    }
-  }
+bool Discard(int arr[],int size){
+  	size_t i;
+  	int val = Rando();
+  	cout << "checking value with" << val << endl;
+  	for(i = 0; i<size; i++){
+    		if(Same(val,arr)){
+      			i = size;
+      			return true;
+    		}
+  	}
 }
-
